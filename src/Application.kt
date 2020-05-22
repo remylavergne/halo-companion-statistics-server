c473102835705ffaad08c65244e67b33b50b0103
Rémy Lavergne <lavergne.remy@gmail.com>
2020-05-22 13:50:35 +0200

    all install into function

--- src/Application.kt
+++ src/Application.kt
@@ -1,8 +1,13 @@
 package dev.remylavergne.halo
 
 import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -16,8 +21,24 @@ fun main(args: Array<String>) {
 
 @Suppress("unused") // Referenced in application.conf
 fun Application.mainModule() {
+    installPlugins(this)
     routing {
        root()
+    }
+}
+
+fun installPlugins(application: Application) {
+    application.install(StatusPages) {
+        exception { cause: Throwable ->
+            call.respond(HttpStatusCode.InternalServerError)
+        }
+    }
+    application.install(DefaultHeaders)
+    application.install(CallLogging)
+    application.install(ContentNegotiation) {
+        gson {
+            // Configure Gson here
+        }
     }
 }
 
@@ -25,11 +46,17 @@ fun Routing.root() {
     get("/") {
         this.call.respond("Hello World!")
     }
+
+    get("/health_check") {
+        // Check databases/other services.
+        call.respondText("OK")
+    }
 }
 
 fun Routing.player() {
     get("/player/{id}") {
-
+        // TODO => Example de récupération d'un paramètre
+        this.call.respond("P")
     }
 }
 
