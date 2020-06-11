601dbe5a36be214f9d4754d0ed824286edff747b
Rémy Lavergne <r.lavergne@afelio.be>
2020-06-11 12:56:40 +0200

    refactor metadata service to include api informations

--- src/services/MetadataServiceImpl.kt
+++ src/services/MetadataServiceImpl.kt
@@ -4,6 +4,7 @@ import dev.remylavergne.halo.data.dto.metadata.*
 import dev.remylavergne.halo.data.enums.Language
 import dev.remylavergne.halo.helpers.MoshiHelper
 import dev.remylavergne.halo.services.interfaces.MetadataService
+import dev.remylavergne.halo.services.interfaces.ResponseListWrapper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
@@ -12,37 +13,37 @@ import okhttp3.Response
 
 class MetadataServiceImpl(private val okHttpClient: OkHttpClient) : MetadataService {
 
-    override suspend fun getCampaignMissions(language: Language): List<CampaignMissionsDto> {
+    override suspend fun getCampaignMissions(language: Language): ResponseListWrapper<List<CampaignMissionsDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/campaign-missions"
         return getList<CampaignMissionsDto>(url, language)
     }
 
-    override suspend fun getCommendations(language: Language): List<CommendationsDto> {
+    override suspend fun getCommendations(language: Language): ResponseListWrapper<List<CommendationsDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/commendations"
         return getList<CommendationsDto>(url, language)
     }
 
-    override suspend fun getCompanyCommendations(language: Language): List<CommendationsDto> {
+    override suspend fun getCompanyCommendations(language: Language): ResponseListWrapper<List<CommendationsDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/company-commendations"
         return getList<CommendationsDto>(url, language)
     }
 
-    override suspend fun getCsrDesignations(language: Language): List<CsrDesignationsDto> {
+    override suspend fun getCsrDesignations(language: Language): ResponseListWrapper<List<CsrDesignationsDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/csr-designations"
         return getList<CsrDesignationsDto>(url, language)
     }
 
-    override suspend fun getEnemies(language: Language): List<EnemiesDto> {
+    override suspend fun getEnemies(language: Language): ResponseListWrapper<List<EnemiesDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/enemies"
         return getList<EnemiesDto>(url, language)
     }
 
-    override suspend fun getFlexibleStats(language: Language): List<FlexibleStatsDto> {
+    override suspend fun getFlexibleStats(language: Language): ResponseListWrapper<List<FlexibleStatsDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/flexible-stats"
         return getList<FlexibleStatsDto>(url, language)
     }
 
-    override suspend fun getGameBaseVariants(language: Language): List<GameBaseVariantsDto> {
+    override suspend fun getGameBaseVariants(language: Language): ResponseListWrapper<List<GameBaseVariantsDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/game-base-variants"
         return getList<GameBaseVariantsDto>(url, language)
     }
@@ -52,7 +53,7 @@ class MetadataServiceImpl(private val okHttpClient: OkHttpClient) : MetadataServ
         return getObject<GameVariantsDto>(url, language)
     }
 
-    override suspend fun getImpulse(language: Language): List<ImpulseDto> {
+    override suspend fun getImpulse(language: Language): ResponseListWrapper<List<ImpulseDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/game-base-variants"
         return getList<ImpulseDto>(url, language)
     }
@@ -62,22 +63,22 @@ class MetadataServiceImpl(private val okHttpClient: OkHttpClient) : MetadataServ
         return getObject<MapVariants>(url, language)
     }
 
-    override suspend fun getMap(language: Language): List<MapDto> {
+    override suspend fun getMap(language: Language): ResponseListWrapper<List<MapDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/maps"
         return getList<MapDto>(url, language)
     }
 
-    override suspend fun getMedals(language: Language): List<MedalDto> {
+    override suspend fun getMedals(language: Language): ResponseListWrapper<List<MedalDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/medals"
         return getList<MedalDto>(url, language)
     }
 
-    override suspend fun getPlaylists(language: Language): List<PlaylistDto> {
+    override suspend fun getPlaylists(language: Language): ResponseListWrapper<List<PlaylistDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/playlists"
         return getList<PlaylistDto>(url, language)
     }
 
-    override suspend fun getRequisitions(id: String, language: Language): List<RequisitionDto> {
+    override suspend fun getRequisitions(id: String, language: Language): ResponseListWrapper<List<RequisitionDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/requisitions/$id"
         return getList<RequisitionDto>(url, language)
     }
@@ -87,32 +88,32 @@ class MetadataServiceImpl(private val okHttpClient: OkHttpClient) : MetadataServ
         return getObject<RequisitionPacksDto>(url, language)
     }
 
-    override suspend fun getSeasons(language: Language): List<SeasonDto> {
+    override suspend fun getSeasons(language: Language): ResponseListWrapper<List<SeasonDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/seasons"
         return getList<SeasonDto>(url, language)
     }
 
-    override suspend fun getSkulls(language: Language): List<SkullDto> {
+    override suspend fun getSkulls(language: Language): ResponseListWrapper<List<SkullDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/skulls"
         return getList<SkullDto>(url, language)
     }
 
-    override suspend fun getSpartanRanks(language: Language): List<SpartanRankDto> {
+    override suspend fun getSpartanRanks(language: Language): ResponseListWrapper<List<SpartanRankDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/spartan-ranks"
         return getList<SpartanRankDto>(url, language)
     }
 
-    override suspend fun getTeamColors(language: Language): List<TeamColorDto> {
+    override suspend fun getTeamColors(language: Language): ResponseListWrapper<List<TeamColorDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/team-colors"
         return getList<TeamColorDto>(url, language)
     }
 
-    override suspend fun getVehicules(language: Language): List<VehiculesDto> {
+    override suspend fun getVehicules(language: Language): ResponseListWrapper<List<VehiculesDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/vehicles"
         return getList<VehiculesDto>(url, language)
     }
 
-    override suspend fun getWeapons(language: Language): List<WeaponDto> {
+    override suspend fun getWeapons(language: Language): ResponseListWrapper<List<WeaponDto>>? {
         val url = "https://www.haloapi.com/metadata/h5/metadata/weapons"
         return getList<WeaponDto>(url = url, language = language)
     }
@@ -138,7 +139,7 @@ class MetadataServiceImpl(private val okHttpClient: OkHttpClient) : MetadataServ
         }
     }
 
-    private suspend inline fun <reified T> getList(url: String, language: Language): List<T> {
+    private suspend inline fun <reified T> getList(url: String, language: Language): ResponseListWrapper<List<T>>? {
         val request: Request =
             Request.Builder()
                 .url(url)
@@ -150,13 +151,16 @@ class MetadataServiceImpl(private val okHttpClient: OkHttpClient) : MetadataServ
                 val response: Response = okHttpClient.newCall(request).execute()
                 val listAdapter = MoshiHelper.getListAdapter(T::class.java)
                 response.body?.string()?.let {
-                    return@withContext listAdapter.fromJson(it)
+                    return@withContext ResponseListWrapperImpl(
+                        ApiHeadersImpl(response.headers, language, url),
+                        listAdapter.fromJson(it)
+                    )
                 }
             }
         } catch (error: Throwable) {
             println(error)
-            emptyList<T>()
-        } ?: emptyList<T>()
+            null
+        }
     }
 
     override suspend fun retrieveEveryMetadata(get: suspend MetadataService.() -> Unit) {
--- src/services/interfaces/ApiHeaders.kt
+++ src/services/interfaces/ApiHeaders.kt
@@ -1,4 +1,10 @@
 package dev.remylavergne.halo.services.interfaces
 
 interface ApiHeaders {
+    var apiVersion: String
+    var apiInstance: String
+    var apiActivityId: String
+    var apiRequestUrl: String
+    var apiLanguage: String
+    var apiRequestDate: String
 }
